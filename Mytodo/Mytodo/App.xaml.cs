b229b7ad0e45810bb1900b3551b678cf4bee8407
wsheat <wangsihao.love@gmail.com>
2023-01-08 08:17:26 +0000

    test

---
+++ Mytodo/Mytodo/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Prism.DryIoc;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Mytodo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+    }
+}
